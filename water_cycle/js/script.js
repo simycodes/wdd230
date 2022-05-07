508cdbfbc88202b39c47576fe0ab7454d04f330b
simycodes <simycodes@gmail.com>
2022-05-07 22:15:41 +0200

    water cycle project

---
+++ water_cycle/js/script.js
@@ -0,0 +1,13 @@
+const activePage = window.location.pathname;
+//console.log(activePage);
+
+const navLinks = document.querySelectorAll("nav a").forEach(link =>{
+    console.log(link.href);
+    if(link.href.includes(`${activePage}`)){
+        //console.log(`$activePage`);
+        link.classList.add("active");
+    }
+})
+
+
+
