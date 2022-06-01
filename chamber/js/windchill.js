0c04437b47837bde14261f3378276d1de1090a11
simycodes <simycodes@gmail.com>
2022-06-01 13:07:24 +0200

    real text content addition,improved design,responsive design,w5-6

---
+++ chamber/js/windchill.js
@@ -0,0 +1,21 @@
+
+// The value obtained from the index page is a number String,its needs to be converted 
+//into a Float, hence the parseFloat()
+const temp = document.querySelector("#temperature").textContent;
+const temperature = parseFloat(temp);
+
+const windSpeed = parseFloat(document.querySelector("#wind-speed").textContent);
+
+let windChill;
+
+if(temperature <= 50 && windSpeed > 3) {
+    windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed,0.16)) + (0.4275 * temperature * Math.pow(windSpeed,0.16))
+    windChill = Math.round(windChill);
+    document.querySelector("#wind-chill").textContent = windChill+"\xB0F";
+    //document.querySelector("#wind-chill").textContent = windChill.toFixed(3);
+}
+else {
+    windChill = "N/A";
+    document.querySelector("#wind-chill").textContent = windChill;
+}
+
