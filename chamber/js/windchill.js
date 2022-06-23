1fb4e917219da2f38f7d9bf119c97881009269e0
simycodes <simycodes@gmail.com>
2022-06-23 19:01:21 +0200

    insertion of weather data on chamber site

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -8,7 +8,8 @@ const windSpeed = parseFloat(document.querySelector("#wind-speed").textContent);
 
 let windChill;
 
-if(temperature <= 50 && windSpeed > 3) {
+//since temperature is coming in celsius we use less 10 if F we use 50
+if(temperature <= 10 && windSpeed > 3) {
     windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed,0.16)) + (0.4275 * temperature * Math.pow(windSpeed,0.16))
     windChill = Math.round(windChill);
     document.querySelector("#wind-chill").textContent = windChill+"\xB0F";
