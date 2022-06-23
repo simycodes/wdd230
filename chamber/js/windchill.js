37b0deac3b68450bb1d249798943411a5feda0b9
simycodes <simycodes@gmail.com>
2022-06-23 20:54:33 +0200

    weather change in js

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,3 +1,34 @@
+const apiURL = "//api.openweathermap.org/data/2.5/weather?id=901344&appid=5dc3e352a835ebf42d11b3813df14eb0&units=metric";
+
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+    
+    document.getElementById("condition").innerHTML = weatherInfo.weather[0].description;
+	document.getElementById("temperature").innerHTML = weatherInfo.main.temp;
+	document.getElementById("wind-speed").innerHTML = weatherInfo.wind.speed;
+
+    const weatherImage = document.getElementById("weather-icon");
+	
+	//getting and displaying the image
+	const iconcode = weatherInfo.weather[0].icon;
+	console.log(iconcode);
+	const icon_path = "//api.openweathermap.org/img/w/"+ iconcode +".png";
+	console.log(icon_path);
+
+    weatherImage.src = icon_path;
+    document.getElementById("weather-icon").src = icon_path;
+    //weatherImage.alt = document.getElementById("condition").innerHTML = weatherInfo.weather[0].description;
+	// document.getElementById("weather-icon").src = icon_path;
+	
+ }); 
+
+
+//  CALCULATING THE WIND CHILL
 
 // The value obtained from the index page is a number String,its needs to be converted 
 //into a Float, hence the parseFloat()
