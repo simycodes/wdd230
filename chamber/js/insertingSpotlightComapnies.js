6e3fc0067be2208644f2e23cda1c09209f239187
simycodes <simycodes@gmail.com>
2022-06-29 20:04:17 +0200

    dyanamic displaying of companies on spotlight area

--- chamber/js/insertingSpotlightComapnies.js
+++ chamber/js/insertingSpotlightComapnies.js
@@ -10,16 +10,68 @@ fetch(requestURL).then(function (response) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     
     const companies = jsonObject['companies'];
-    companies.forEach(displayCompanies);
-    //prophets.forEach(displayProphets);
+    //companies.forEach(displayCompanies);
+    displaySpotlightCompanies(companies);
 });
 
 
+function  displaySpotlightCompanies(companies){
+    let goldCompanies = [];
+    let randomCompanyNumber = 0;
+    let randomCompanyDisplay = "";
+
+    console.log("Displayin companies inside displaySpotlightCompanies function");
+    console.log(companies);
+    console.log("Just consoled the companies above")
+
+    for (let i = 0; i < companies.length; i++) {
+        if(companies[i].membershiplevel === "Gold"){
+            console.log("inside the for loop")
+            console.log(companies[i].name);
+            goldCompanies.push(companies[i]);
+        }
+    }
+
+    console.log("Displaying Gold companies");
+    console.log(goldCompanies);
+    console.log("Just Displayed Gold companies");
+
+    const headingSpotlightOne = document.getElementById("heading-spotlight-one");
+    const imageSpotlightOne = document.getElementById("image-spotlight-one");
+    const paragraphSpotlightOne = document.getElementById("paragraph-spotlight-one");
+
+    const headingSpotlightTwo = document.getElementById("heading-spotlight-two");
+    const imageSpotlightTwo = document.getElementById("image-spotlight-two");
+    const paragraphSpotlightTwo = document.getElementById("paragraph-spotlight-two");
+
+    //Displaying the first random company
+    randomCompanyNumber = Math.floor(Math.random() * goldCompanies.length);
+    randomCompanyDisplay = goldCompanies[randomCompanyNumber];
+
+    console.log("Displaying a random gold company");
+    console.log(randomCompanyDisplay.name);
+
+    imageSpotlightOne.setAttribute('src', randomCompanyDisplay.image);
+    imageSpotlightOne.setAttribute("alt",`Company logo image of ${randomCompanyDisplay.name}`);
+
+    //Displaying second random company
+    randomCompanyNumber = Math.floor(Math.random() * goldCompanies.length);
+    randomCompanyDisplay = goldCompanies[randomCompanyNumber]
+
+    console.log("Displaying a second random gold company");
+    console.log(randomCompanyDisplay.name);
+
+    imageSpotlightTwo.setAttribute('src', randomCompanyDisplay.image);
+    imageSpotlightTwo.setAttribute("alt",`Company logo image of ${randomCompanyDisplay.name}`);
+}
+
+// function showCompany() {
+// }
 
 function displayCompanies(company) {
 
   //Setting spotlight information for 2 gold companies
-  if(company.membershiplevel === "Gold" && company.name === "Ndola Chamber of Commerce") {
+  if(company.membershiplevel === "Gold" && company.name === "Zamtel") {
     const headingSpotlightOne = document.getElementById("heading-spotlight-one");
     const imageSpotlightOne = document.getElementById("image-spotlight-one");
     const paragraphSpotlightOne = document.getElementById("paragraph-spotlight-one");
@@ -38,6 +90,9 @@ function displayCompanies(company) {
   }
 
 }
+
+
+//----------------------------------------------------------------------------------------------------
 
 
 // function displayCompanySpotLights(company) {
