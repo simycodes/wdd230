5f257a3f2115cf4d6da267827d2ef15ff40195ac
simycodes <simycodes@gmail.com>
2022-06-30 11:53:27 +0200

    Updated json data for spotlight gold companies

--- chamber/js/insertingSpotlightComapnies.js
+++ chamber/js/insertingSpotlightComapnies.js
@@ -51,6 +51,8 @@ function  displaySpotlightCompanies(companies){
     console.log("Displaying a random gold company");
     console.log(randomCompanyDisplay.name);
 
+    headingSpotlightOne.textContent = randomCompanyDisplay.name;
+    paragraphSpotlightOne.textContent = randomCompanyDisplay.message;
     imageSpotlightOne.setAttribute('src', randomCompanyDisplay.image);
     imageSpotlightOne.setAttribute("alt",`Company logo image of ${randomCompanyDisplay.name}`);
 
@@ -61,72 +63,9 @@ function  displaySpotlightCompanies(companies){
     console.log("Displaying a second random gold company");
     console.log(randomCompanyDisplay.name);
 
+    headingSpotlightTwo.textContent = randomCompanyDisplay.name;
+    paragraphSpotlightTwo.textContent = randomCompanyDisplay.message;
     imageSpotlightTwo.setAttribute('src', randomCompanyDisplay.image);
     imageSpotlightTwo.setAttribute("alt",`Company logo image of ${randomCompanyDisplay.name}`);
 }
 
-// function showCompany() {
-// }
-
-function displayCompanies(company) {
-
-  //Setting spotlight information for 2 gold companies
-  if(company.membershiplevel === "Gold" && company.name === "Zamtel") {
-    const headingSpotlightOne = document.getElementById("heading-spotlight-one");
-    const imageSpotlightOne = document.getElementById("image-spotlight-one");
-    const paragraphSpotlightOne = document.getElementById("paragraph-spotlight-one");
-
-    imageSpotlightOne.setAttribute('src', company.image);
-    imageSpotlightOne.setAttribute("alt",`Company logo image of ${company.name}`);
-  }
-
-  if(company.membershiplevel === "Gold" && company.name === "Mwaiseni Maisonettes") {
-    const headingSpotlightTwo = document.getElementById("heading-spotlight-two");
-    const imageSpotlightTwo = document.getElementById("image-spotlight-two");
-    const paragraphSpotlightTwo = document.getElementById("paragraph-spotlight-two");
-
-    imageSpotlightTwo.setAttribute('src', company.image);
-    imageSpotlightTwo.setAttribute("alt",`Company logo image of ${company.name}`);
-  }
-
-}
-
-
-//----------------------------------------------------------------------------------------------------
-
-
-// function displayCompanySpotLights(company) {
-//   const headingSpotlightOne = document.getElementById("heading-spotlight-one");
-//   const imageSpotlightOne = document.getElementById("image-spotlight-one");
-//   const paragraphSpotlightOne = document.getElementById("paragraph-spotlight-one");
-
-//   imageSpotlightOne.setAttribute('src', company.image);
-//   imageSpotlightOne.setAttribute("alt",`Company logo image of ${company.name}`);
-
-//   const headingSpotlightTwo = document.getElementById("heading-spotlight-two");
-//   const imageSpotlightTwo = document.getElementById("image-spotlight-two");
-//   const paragraphSpotlightTwo = document.getElementById("paragraph-spotlight-two");
-
-//   imageSpotlightTwo.setAttribute('src', company.image);
-//   imageSpotlightTwo.setAttribute("alt",`Company logo image of ${company.name}`);
-
-// }
-
-
-
- //Setting spotlight information for 2 gold companies
-  // if(company.membershiplevel === "Gold" && company.name === "Zamtel") {
-  //   const headingSpotlightOne = document.getElementById("heading-spotlight-one");
-  //   const imageSpotlightOne = document.getElementById("image-spotlight-one");
-  //   const paragraphSpotlightOne = document.getElementById("paragraph-spotlight-one");
-
-  //   imageSpotlightOne.setAttribute('src', company.image);
-  //   imageSpotlightOne.setAttribute("alt",`Company logo image of ${company.name}`);
-
-  //   const headingSpotlightTwo = document.getElementById("heading-spotlight-two");
-  //   const imageSpotlightTwo = document.getElementById("image-spotlight-two");
-  //   const paragraphSpotlightTwo = document.getElementById("paragraph-spotlight-two");
-
-  //   imageSpotlightTwo.setAttribute('src', company.image);
-  //   imageSpotlightTwo.setAttribute("alt",`Company logo image of ${company.name}`);
-  // }
